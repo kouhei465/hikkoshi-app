c7bd27340cdfb9eb768e08f2896c02f5890636f2
kouhei465 <kouhei-0611@outlook.jp>
2026-05-27 05:03:23 +0900

    ログアウト機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
 
+  delete "logout", to: "user_sessions#destroy"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
