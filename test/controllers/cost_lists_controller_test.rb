02ef25748c47de766c04ddfbae3bc22d283905d7
kouhei465 <kouhei-0611@outlook.jp>
2026-06-20 06:19:13 +0900

    MVP前のView調整と候補付き自由入力を追加

--- test/controllers/cost_lists_controller_test.rb
+++ test/controllers/cost_lists_controller_test.rb
@@ -5,4 +5,16 @@ class CostListsControllerTest < ActionDispatch::IntegrationTest
     get new_cost_list_url
     assert_response :success
   end
+
+  test "renders one name datalist per category and text fields referencing it" do
+    get new_cost_list_url
+
+    CostItem.categories.each_key do |category|
+      assert_select "datalist#cost-item-name-options-#{category}", count: 1
+      assert_select "input[list='cost-item-name-options-#{category}']", count: 2
+    end
+
+    assert_select "select[name$='[name]']", count: 0
+    assert_select "[data-nested-form-target='customNameEstimateMessage']"
+  end
 end
