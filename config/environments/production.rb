03d2033c2d1dd38fb9b639c1a2d06659f1c59b65
kouhei465 <kouhei-0611@outlook.jp>
2026-08-08 03:05:52 +0900

    Resend APIによるメール送信に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,15 +79,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "hikkoshi-cost.com"), protocol: "https" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.resend.com",
-    port: 587,
-    user_name: "resend",
-    password: ENV["RESEND_API_KEY"],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :resend
   config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
